065de1effa108b60a7a170f818d7348a8f8ef6df
Ahmed Gebril <ahmedgebril1889@gmail.com>
2023-05-05 16:54:11 -0700

    got the rateLimit to work had an issue with the function

--- my-app/server_side/middleware/rateLimit/rateLimit.js
+++ my-app/server_side/middleware/rateLimit/rateLimit.js
@@ -1,13 +1,13 @@
 const rateLimit = require('express-rate-limit')
 
-  function getRateLimit(){
-    rateLimit = rateLimit({
-      windowMs: 60 * 1000, // 1 minute
-      max: 100, 
-      message: 'Too many requests from this IP, please try again later',
-    })
-  }
-  
-  module.exports=getRateLimit
+function getRateLimit(req, res, next) {
+  const limiter = rateLimit({
+    windowMs: 60 * 1000, // 1 minute
+    max: 100, 
+    message: 'Too many requests from this IP, please try again later',
+  })
+  console.log('here')
+  limiter(req, res, next)
+}
 
-  
+module.exports = getRateLimit
